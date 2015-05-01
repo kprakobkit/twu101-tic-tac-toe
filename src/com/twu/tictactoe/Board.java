7a5068e5c9fab2d0563a456bea0fca7b3a56484e
kprakobkit <kprakobkit@gmail.com>
2015-04-30 20:22:43 -0500

    Remove redraw()

--- src/com/twu/tictactoe/Board.java
+++ src/com/twu/tictactoe/Board.java
@@ -31,16 +31,11 @@ public class Board {
         printStream.println(boardString);
     }
 
-    public void redraw(String currentUser, String positionOnBoard) {
-        updatePlayerPosition(currentUser, positionOnBoard);
-        printBoard();
-    }
-
     public boolean isAValidPosition(String position) {
         return !(playerPositions[Integer.parseInt(position) - 1].equals("  "));
     };
 
-    private void updatePlayerPosition(String currentUser, String positionOnBoard) {
+    public void updatePlayerPosition(String currentUser, String positionOnBoard) {
         String mark = currentUser.equals("1") ? " X " : " O ";
         playerPositions[Integer.parseInt(positionOnBoard) - 1] = mark;
     }
--- src/com/twu/tictactoe/Game.java
+++ src/com/twu/tictactoe/Game.java
@@ -33,8 +33,8 @@ public class Game {
                 printStream.println("The position is taken. Please enter another position.");
                 userInput = gameHelper.askForUserInput(currentPlayer);
             }
-            board.redraw(currentPlayer, userInput);
-
+            board.updatePlayerPosition(currentPlayer, userInput);
+            board.printBoard();
             switchCurrentPlayer();
         }
     }
